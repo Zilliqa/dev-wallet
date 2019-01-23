3446f29d11f79c1b9ece205a8b080644d98031b7
noelyoo <innovation.noel@gmail.com>
2019-01-23 16:53:24 +0800

    feat(header): add sign out button

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,34 +1,61 @@
 import React from 'react';
 import { withRouter } from 'react-router';
 import { connect } from 'react-redux';
-import { Navbar, NavbarBrand } from 'reactstrap';
+import { Navbar, NavItem, NavLink, NavbarBrand } from 'reactstrap';
 import * as H from 'history';
 import './style.css';
+import Nav from 'reactstrap/lib/Nav';
+import * as zilActions from '../../redux/zil/actions';
+import { requestStatus } from '../../constants';
 
 interface IProps {
   history: H.History;
   location: H.Location;
   network: string;
+  authStatus?: string;
+  clear: () => void;
 }
 
 class Header extends React.Component<IProps, {}> {
   public render() {
+    const { authStatus } = this.props;
     return (
       <div>
         <Navbar fixed={'top'} dark={true} expand="md" color="faded">
           <NavbarBrand href="/">{'Nucleus Wallet'}</NavbarBrand>
           <div className="network">{this.props.network} network</div>
+          {authStatus === requestStatus.SUCCEED ? (
+            <Nav className="ml-auto" navbar={true} style={{ marginRight: 150 }}>
+              <NavItem>
+                <NavLink style={{ cursor: 'pointer' }} onClick={this.signOut}>
+                  {'Sign Out'}
+                </NavLink>
+              </NavItem>
+            </Nav>
+          ) : null}
         </Navbar>
       </div>
     );
   }
+
+  private signOut = () => {
+    this.props.clear();
+  };
 }
 
 // @ts-ignore
 const HeaderWithRouter = withRouter(Header);
 
 const mapStateToProps = (state) => ({
-  network: state.zil.network
+  network: state.zil.network,
+  authStatus: state.zil.authStatus
 });
 
-export default connect(mapStateToProps)(HeaderWithRouter);
+const mapDispatchToProps = (dispatch) => ({
+  clear: () => dispatch(zilActions.clear())
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(HeaderWithRouter);
