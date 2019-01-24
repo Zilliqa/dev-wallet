80b5926549761a867d54acb01643c7a5aa533bab
noelyoo <innovation.noel@gmail.com>
2019-01-25 00:47:37 +0900

    refactor: change node url

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -6,7 +6,7 @@ export enum requestStatus {
 
 export const CAPTCHA_SITE_KEY = '6LdazYoUAAAAAIJDC8m5PSMp2mcmSmzgt7pxU6IG';
 
-export const NODE_URL = 'https://t341b-api.aws.zilliqa.com';
+export const NODE_URL = 'https://non-contract-api.aws.z7a.xyz';
 export const CHAIN_ID = 2;
 export const MSG_VERSION = 1;
 export const NETWORK = 'testnetv3';
