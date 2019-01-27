1e2bdb8b43c8f4e62f852b8b7f8355b5a611fad4
noelyoo <innovation.noel@gmail.com>
2019-01-27 12:20:49 +0900

    feat(home): add disclaimer

--- src/containers/HomeContainer.tsx
+++ src/containers/HomeContainer.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Layout from '../components/layout';
 import { NODE_URL, CHAIN_ID, MSG_VERSION } from '../constants';
+import Row from 'reactstrap/lib/Row';
+import Col from 'reactstrap/lib/Col';
+import { MdSecurity, MdBeenhere } from 'react-icons/md';
 
 class Home extends React.Component {
   public render() {
@@ -14,19 +17,57 @@ class Home extends React.Component {
                 <p className="pt-2">
                   Nucleus Wallet is <b>testnet-based</b> wallet.
                   <br />
-                  Kindly use this wallet for <b>testing purpose only</b>.
+                  <small>
+                    WARNING: This is a Zilliqa testnet. Please do not send any interim ERC-20 ZIL
+                    tokens to this wallet.
+                  </small>
                   <br />
                 </p>
               </div>
             </div>
           </div>
-          <br />
-          <div className="blockchain-detail">
-            <small>Chain ID: {CHAIN_ID}</small>
+          <div className="node-info text-center text-fade-in">
+            <small>{`Chain ID: ${CHAIN_ID}`}</small>
             {' | '}
-            <small>Msg Ver: {MSG_VERSION}</small>
+            <small>{`Msg Ver: ${MSG_VERSION}`}</small>
             {' | '}
-            <small>Node URL: {NODE_URL}</small>
+            <small>{`Node URL: ${NODE_URL}`}</small>
+          </div>
+          <div className="container pt-5">
+            <Row>
+              <Col xs={6} sm={6} md={6} lg={4} className="text-center ml-auto">
+                <MdSecurity size={50} />
+                <div className="text-secondary text-center pt-3">
+                  <b>
+                    We do not store your private key on our servers or transmit it over the network
+                    at any time
+                  </b>
+                  <br />
+                  <p className="pt-1">
+                    <small>
+                      The Nucleus Wallet is free, open-source and runs entirely on your computer.
+                      Private key generation and keystore encryption/decryption are handled on your
+                      computer only.
+                    </small>
+                  </p>
+                </div>
+              </Col>
+              <Col xs={6} sm={6} md={6} lg={4} className="text-center mr-auto">
+                <MdBeenhere size={50} />
+                <div className="text-secondary text-center pt-3">
+                  <b>We recommend downloading and securely storing a keystore json file.</b>
+                  <br />
+                  <p className="pt-1">
+                    <small>
+                      keystore json file contains your encrypted private key, which can only be
+                      decrypted with a password you choose. You are solely responsible for your
+                      account funds. If you lose your private key or the password to your keystore
+                      json, it cannot be recovered by anyone.
+                    </small>
+                  </p>
+                </div>
+              </Col>
+            </Row>
           </div>
         </Layout>
       </div>
