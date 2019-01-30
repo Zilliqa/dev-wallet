47adff55728a12e4a18e7fc7135e41a96d150af8
noelyoo <innovation.noel@gmail.com>
2019-01-31 00:00:18 +0900

    feat(disclaimer): remove bold tag

--- src/components/disclaimer/index.tsx
+++ src/components/disclaimer/index.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 export const Disclaimer: React.SFC = () => (
   <small className="text-danger">
-    <b>
-      This is Zilliqa Test Net wallet. Do not send any interim ERC-20 ZIL tokens to this wallet. You
-      are solely responsible for your account funds.
-    </b>
+    This is Zilliqa Test Net wallet. Do not send any interim ERC-20 ZIL tokens to this wallet. You
+    are solely responsible for your account funds.
   </small>
 );
