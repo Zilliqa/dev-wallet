8da71c266f6857cfa7244f7f266f20bf91b6634c
noelyoo <innovation.noel@gmail.com>
2019-02-07 12:58:19 +0900

    refactor(faucet): use react hooks

--- src/components/faucet-form/index.tsx
+++ src/components/faucet-form/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Card, Row, Col } from 'reactstrap';
 import { BN, units } from '@zilliqa-js/util';
 import * as zilActions from '../../redux/zil/actions';
@@ -25,165 +25,169 @@ interface IState {
   isFaucetComplete: boolean;
   isFaucetIncomplete: boolean;
   balance: string;
+  prevFaucetStatus?: string;
 }
 
-const initialState = {
+const initialState: IState = {
   isRunningFaucet: false,
-  isUpdatingBalance: false,
   isFaucetComplete: false,
   isFaucetIncomplete: false,
-  balance: '0'
+  isUpdatingBalance: false,
+  balance: '0',
+  prevFaucetStatus: requestStatus.PENDING
 };
-class FaucetForm extends React.Component<IProps, IState> {
-  public readonly state: IState = initialState;
-  public componentDidMount() {
-    this.getBalance();
-  }
 
-  public componentWillReceiveProps(nextProps) {
-    const isAccessFailed =
-      nextProps.faucetStatus === requestStatus.FAILED &&
-      this.props.faucetStatus === requestStatus.PENDING;
-    const isAccessSucceeded =
-      nextProps.faucetStatus === requestStatus.SUCCEED &&
-      this.props.faucetStatus === requestStatus.PENDING;
+const FaucetForm: React.FunctionComponent<IProps> = (props) => {
+  const { zilliqa, address, network, faucetTxId, faucetStatus } = props;
 
-    if (isAccessFailed) {
-      this.setState({ isRunningFaucet: false, isFaucetIncomplete: true, isFaucetComplete: false });
-    } else if (isAccessSucceeded) {
-      this.setState(
-        { isRunningFaucet: false, isFaucetComplete: true, isFaucetIncomplete: false },
-        this.getBalance
-      );
-    }
-  }
+  const [isUpdatingBalance, setIsUpdatingBalance] = useState(initialState.isUpdatingBalance);
+  const [balance, setBalance] = useState(initialState.balance);
+  useEffect(
+    () => {
+      getBalance();
+    },
+    [balance]
+  );
 
-  public render() {
-    const { address, network, faucetTxId } = this.props;
-    const {
-      isUpdatingBalance,
-      balance,
-      isRunningFaucet,
-      isFaucetComplete,
-      isFaucetIncomplete
-    } = this.state;
-
-    return (
-      <div>
-        <AccountInfo
-          address={address}
-          balance={balance}
-          getBalance={this.getBalance}
-          isUpdatingBalance={isUpdatingBalance}
-        />
-        <Row className="pt-4">
-          <Col xs={12} sm={12} md={10} lg={9} className="mr-auto">
-            <Card>
-              <div className="py-5">
-                <div className="px-4 text-center">
-                  <h2 className="pb-2">
-                    <b>{'ZIL Faucet'}</b>
-                  </h2>
-                  <p className="text-secondary">
-                    {`This Zil faucet is running on The ${network} Network.`}
-
-                    <br />
-                    {'Please run the faucet to receive a small amount of Zil for testing.'}
-                  </p>
-                  <div className="py-4">
-                    {isRunningFaucet || isFaucetComplete ? (
-                      <div>
-                        <SpinnerWithCheckMark loading={isRunningFaucet} />
-                        {isRunningFaucet ? (
-                          <div className="text-center py-4">
-                            <p className="text-secondary text-fade-in">
-                              {'Running Faucet'}
-                              <br />
-                              <small>{'Please kindly wait. It might take a while.'}</small>
-                            </p>
-                          </div>
-                        ) : null}
-                        {isFaucetComplete ? (
-                          <div>
-                            <p className="pt-4 text-secondary">
-                              <span className="text-primary">{'Transaction In Process'}</span>
-                              <br />
-                              <br />
-                              <small>
-                                {'Your transaction is pending blockchain confirmation.'}
-                              </small>
-                              <br />
-                              <small>{'Please check after a few minutes.'}</small>
-                            </p>
-                            {faucetTxId ? (
-                              <u>
-                                <a
-                                  target="_blank"
-                                  href={`${EXPLORER_URL}/transactions/${faucetTxId}`}
-                                  rel="noreferrer"
-                                >
-                                  {'View Your Transaction'}
-                                </a>
-                              </u>
-                            ) : null}
-                          </div>
-                        ) : null}
-                      </div>
-                    ) : (
-                      <div className="recaptcha">
-                        <ReCAPTCHA
-                          sitekey={CAPTCHA_SITE_KEY}
-                          onChange={this.handleCaptcha}
-                          badge="inline"
-                        />
-                        {isFaucetIncomplete ? (
-                          <p className="pt-4">
-                            <small className="text-danger text-fade-in">
-                              {'Failed to run faucet. Please try again later.'}
-                            </small>
-                          </p>
-                        ) : null}
-                      </div>
-                    )}
-                  </div>
-                </div>
-              </div>
-            </Card>
-          </Col>
-        </Row>
-      </div>
-    );
-  }
-
-  private handleCaptcha = (token) => {
-    const { address } = this.props;
-    const callback = () => this.props.runFaucet(address, token);
-    this.setState({ isRunningFaucet: true }, callback);
-  };
-
-  private getBalance = async () => {
-    const { zilliqa, address } = this.props;
-    this.setState({ isUpdatingBalance: true });
+  const getBalance = async () => {
+    setIsUpdatingBalance(true);
     try {
       const response = await zilliqa.blockchain.getBalance(address);
 
       if (response.error) {
         console.log(response);
-        this.setState({ isUpdatingBalance: false });
+        setIsUpdatingBalance(false);
       } else {
         if (response.result) {
           const balanceInQa = response.result.balance;
           const balanceInZil = units.fromQa(new BN(balanceInQa), units.Units.Zil); // Sending an amount measured in Zil, converting to Qa.
-
-          this.setState({ balance: balanceInZil, isUpdatingBalance: false });
+          setBalance(balanceInZil);
+          setIsUpdatingBalance(false);
         }
       }
     } catch (error) {
       console.log(error);
-      this.setState({ isUpdatingBalance: false });
+      setIsUpdatingBalance(false);
     }
   };
-}
+
+  const [isFaucetComplete, setIsFaucetComplete] = useState(initialState.isFaucetComplete);
+  const [isFaucetIncomplete, setIsFaucetIncomplete] = useState(initialState.isFaucetIncomplete);
+  const [isRunningFaucet, setIsRunningFaucet] = useState(initialState.isRunningFaucet);
+  const [prevFaucetStatus, setPrevFaucetStatus] = useState(initialState.prevFaucetStatus);
+  useEffect(
+    () => {
+      const isFailed =
+        faucetStatus === requestStatus.FAILED && prevFaucetStatus === requestStatus.PENDING;
+
+      const isSucceeded =
+        faucetStatus === requestStatus.SUCCEED && prevFaucetStatus === requestStatus.PENDING;
+
+      if (isFailed) {
+        setIsRunningFaucet(false);
+        setIsFaucetComplete(false);
+        setIsFaucetIncomplete(true);
+      }
+      if (isSucceeded) {
+        setIsRunningFaucet(false);
+        setIsFaucetComplete(true);
+        setIsFaucetIncomplete(false);
+      }
+
+      setPrevFaucetStatus(faucetStatus);
+    },
+    [faucetStatus, prevFaucetStatus]
+  );
+
+  const handleCaptcha = (token) => {
+    setIsRunningFaucet(true);
+    props.runFaucet(address, token);
+  };
+
+  return (
+    <div>
+      <AccountInfo
+        address={address}
+        balance={balance}
+        getBalance={getBalance}
+        isUpdatingBalance={isUpdatingBalance}
+      />
+      <Row className="pt-4">
+        <Col xs={12} sm={12} md={10} lg={9} className="mr-auto">
+          <Card>
+            <div className="py-5">
+              <div className="px-4 text-center">
+                <h2 className="pb-2">
+                  <b>{'ZIL Faucet'}</b>
+                </h2>
+                <p className="text-secondary">
+                  {`This Zil faucet is running on The ${network} Network.`}
+
+                  <br />
+                  {'Please run the faucet to receive a small amount of Zil for testing.'}
+                </p>
+                <div className="py-4">
+                  {isRunningFaucet || isFaucetComplete ? (
+                    <div>
+                      <SpinnerWithCheckMark loading={isRunningFaucet} />
+                      {isRunningFaucet ? (
+                        <div className="text-center py-4">
+                          <p className="text-secondary text-fade-in">
+                            {'Running Faucet'}
+                            <br />
+                            <small>{'Please kindly wait. It might take a while.'}</small>
+                          </p>
+                        </div>
+                      ) : null}
+                      {isFaucetComplete ? (
+                        <div>
+                          <p className="pt-4 text-secondary">
+                            <span className="text-primary">{'Transaction In Process'}</span>
+                            <br />
+                            <br />
+                            <small>{'Your transaction is pending blockchain confirmation.'}</small>
+                            <br />
+                            <small>{'Please check after a few minutes.'}</small>
+                          </p>
+                          {faucetTxId ? (
+                            <u>
+                              <a
+                                target="_blank"
+                                href={`${EXPLORER_URL}/transactions/${faucetTxId}`}
+                                rel="noreferrer"
+                              >
+                                {'View Your Transaction'}
+                              </a>
+                            </u>
+                          ) : null}
+                        </div>
+                      ) : null}
+                    </div>
+                  ) : (
+                    <div className="recaptcha">
+                      <ReCAPTCHA
+                        sitekey={CAPTCHA_SITE_KEY}
+                        onChange={handleCaptcha}
+                        badge="inline"
+                      />
+                      {isFaucetIncomplete ? (
+                        <p className="pt-4">
+                          <small className="text-danger text-fade-in">
+                            {'Failed to run faucet. Please try again later.'}
+                          </small>
+                        </p>
+                      ) : null}
+                    </div>
+                  )}
+                </div>
+              </div>
+            </div>
+          </Card>
+        </Col>
+      </Row>
+    </div>
+  );
+};
 
 const mapStateToProps = (state) => ({
   faucetTxId: state.zil.faucetTxId,
