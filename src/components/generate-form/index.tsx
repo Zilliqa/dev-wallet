83071684a7dd90610bb3fe9fc7146bf6edc11f1a
noelyoo <noelyoo.dev@gmail.com>
2019-06-02 23:04:30 +0900

    refactor: generate form

--- src/components/generate-form/index.tsx
+++ src/components/generate-form/index.tsx
@@ -32,35 +32,156 @@ const FIRST_STEP = 0;
 const SECOND_STEP = 1;
 const FINAL_STEP = 2;
 
-interface IState {
-  worker: any;
-  isDisclaimerChecked: boolean;
-  passphrase: string;
-  passphraseValid: boolean;
-  passphraseInvalid: boolean;
-  keystoreJSON?: string;
-  currentStep: number;
-  encryptStatus?: string;
-  privateKey?: string;
-}
+const GenerateForm: React.FunctionComponent = () => {
+  const [currentStep, setCurrentStep] = useState(FIRST_STEP);
+  const [privateKey, setPrivateKey] = useState();
+  const [passphrase, setPassphrase] = useState('');
 
-const initialState: IState = {
-  worker: undefined,
-  isDisclaimerChecked: false,
-  passphrase: '',
-  passphraseValid: false,
-  passphraseInvalid: false,
-  encryptStatus: undefined,
-  keystoreJSON: undefined,
-  privateKey: undefined,
-  currentStep: FIRST_STEP
+  return (
+    <div>
+      <Card>
+        <div className="py-5">
+          <Row>
+            <Col xs={11} sm={11} md={10} lg={8} className="mr-auto ml-auto">
+              <Steps size="small" current={currentStep}>
+                <Step title="Passphrase" />
+                <Step title="Keystore" />
+                <Step title="Complete" />
+              </Steps>
+            </Col>
+            <Col xs={10} sm={10} md={8} lg={7} className="mr-auto ml-auto">
+              {currentStep === FIRST_STEP ? (
+                <PassphraseStep
+                  passphrase={passphrase}
+                  setPassphrase={setPassphrase}
+                  setCurrentStep={setCurrentStep}
+                />
+              ) : currentStep === SECOND_STEP ? (
+                <KeystoreStep
+                  passphrase={passphrase}
+                  privateKey={privateKey}
+                  setPrivateKey={setPrivateKey}
+                  setCurrentStep={setCurrentStep}
+                />
+              ) : currentStep === FINAL_STEP ? (
+                <FinalStep privateKey={privateKey} />
+              ) : null}
+            </Col>
+          </Row>
+        </div>
+      </Card>
+    </div>
+  );
 };
 
-const GenerateForm: React.FunctionComponent = (props) => {
-  const [worker, setWorker] = useState(initialState.worker);
-  const [encryptStatus, setEncryptStatus] = useState(initialState.encryptStatus);
-  const [keystoreJSON, setKeystoreJSON] = useState(initialState.keystoreJSON);
-  const [privateKey, setPrivateKey] = useState(initialState.privateKey);
+const PassphraseStep = ({ passphrase, setPassphrase, setCurrentStep }) => {
+  const [passphraseValid, setPassphraseValid] = useState(false);
+  const [passphraseInvalid, setPassphraseInvalid] = useState(false);
+  const [isDisclaimerChecked, setIsDisclaimerChecked] = useState(false);
+  const handleCheck = () => {
+    setIsDisclaimerChecked(!isDisclaimerChecked);
+  };
+
+  const changePassphrase = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    e.preventDefault();
+    const value = e.target.value;
+    const key = 'passphrase';
+    const validationResult: any = getInputValidationState(key, value, /^.{8,}$/);
+    setPassphrase(value);
+    setPassphraseValid(validationResult.passphraseValid);
+    setPassphraseInvalid(validationResult.passphraseInvalid);
+  };
+
+  const isDisabled = !passphraseValid || !isDisclaimerChecked;
+
+  return (
+    <div>
+      <div className="text-center">
+        <h2 className="pt-5">
+          <b>{'Set Passphrase for your Keystore File'}</b>
+        </h2>
+        <p className="text-secondary py-3">
+          {`This is your first step in creating your Klaytn Wallet.`}
+          <br />
+          {`Please set the password for the keystore file for your new wallet.`}
+        </p>
+      </div>
+      <div>
+        <Form className="mt-4" onSubmit={(e) => e.preventDefault()}>
+          <FormGroup>
+            <Label for="passphrase">
+              <small>
+                <b>{'Passphrase'}</b>
+              </small>
+            </Label>
+            <Input
+              id="passphrase"
+              type="password"
+              name="passphrase"
+              data-testid="passphrase"
+              value={passphrase}
+              onChange={changePassphrase}
+              valid={passphraseValid}
+              invalid={passphraseInvalid}
+              placeholder="Enter the passphrase"
+              // autoComplete="new-password"
+              autoComplete="off"
+              maxLength={32}
+              minLength={8}
+            />
+            <FormFeedback>{'invalid passphrase'}</FormFeedback>
+            <FormFeedback valid={true}>{'valid passphrase'}</FormFeedback>
+          </FormGroup>
+          <br />
+
+          <div className="px-3">
+            <FormGroup inline={true}>
+              <Label check={isDisclaimerChecked} onChange={handleCheck}>
+                <Input type="checkbox" /> <Disclaimer />
+              </Label>
+            </FormGroup>
+            <div className="text-center">
+              <Button
+                text={'Confirm'}
+                type="primary"
+                ariaLabel={'Confirm'}
+                onClick={() => setCurrentStep(SECOND_STEP)}
+                disabled={isDisabled}
+              />
+            </div>
+          </div>
+        </Form>
+      </div>
+    </div>
+  );
+};
+
+const KeystoreStep = ({ setCurrentStep, setPrivateKey, privateKey, passphrase }) => {
+  const [worker, setWorker] = useState();
+  const [encryptStatus, setEncryptStatus] = useState();
+  const [keystoreJSON, setKeystoreJSON] = useState();
+
+  const downloadKeystore = () => {
+    if (typeof keystoreJSON !== 'string') {
+      return;
+    }
+    const keystoreObject = JSON.parse(keystoreJSON);
+    const filename = `zilliqa_keystore_${new Date().toISOString()}`;
+
+    setCurrentStep(FINAL_STEP);
+    setEncryptStatus(requestStatus.SUCCEED);
+    downloadObjectAsJson(keystoreObject, filename);
+  };
+
+  useEffect(
+    () => {
+      if (privateKey && keystoreJSON) {
+        downloadKeystore();
+      }
+    },
+    [privateKey, keystoreJSON]
+  );
+
   useEffect(() => {
     if (worker === undefined) {
       const myWorker = new Worker();
@@ -77,194 +198,62 @@ const GenerateForm: React.FunctionComponent = (props) => {
     }
   });
 
-  useEffect(
-    () => {
-      if (privateKey && keystoreJSON) {
-        downloadKeystore();
-      }
-    },
-    [privateKey, keystoreJSON]
-  );
-
-  const [passphrase, setPassphrase] = useState(initialState.passphrase);
-  const [passphraseValid, setPassphraseValid] = useState(initialState.passphraseValid);
-  const [passphraseInvalid, setPassphraseInvalid] = useState(initialState.passphraseInvalid);
-  const [isDisclaimerChecked, setIsDisclaimerChecked] = useState(initialState.isDisclaimerChecked);
-
-  const [currentStep, setCurrentStep] = useState(initialState.currentStep);
-
-  const handleCheck = () => {
-    setIsDisclaimerChecked(!isDisclaimerChecked);
-  };
-
   const generateKeystore = () => {
     setEncryptStatus(requestStatus.PENDING);
     worker.postMessage({ passphrase });
   };
 
-  const downloadKeystore = () => {
-    if (typeof keystoreJSON !== 'string') {
-      return;
-    }
-    const keystoreObject = JSON.parse(keystoreJSON);
-    const filename = `zilliqa_keystore_${new Date().toISOString()}`;
-
-    setCurrentStep(FINAL_STEP);
-    setEncryptStatus(requestStatus.SUCCEED);
-    downloadObjectAsJson(keystoreObject, filename);
-  };
-
-  const changePassphrase = (e: React.ChangeEvent<HTMLInputElement>): void => {
-    e.preventDefault();
-    const value = e.target.value;
-    const key = 'passphrase';
-    const validationResult: any = getInputValidationState(key, value, /^.{8,}$/);
-    setPassphrase(value);
-    setPassphraseValid(validationResult.passphraseValid);
-    setPassphraseInvalid(validationResult.passphraseInvalid);
-  };
-
-  const renderPassphraseStep = () => {
-    const isDisabled = !passphraseValid || !isDisclaimerChecked;
-
-    return (
-      <div>
-        <div className="text-center">
-          <h2 className="pt-5">
-            <b>{'Set Passphrase for your Keystore File'}</b>
-          </h2>
-          <p className="text-secondary py-3">
-            {`This is your first step in creating your Klaytn Wallet.`}
-            <br />
-            {`Please set the password for the keystore file for your new wallet.`}
-          </p>
-        </div>
-        <div>
-          <Form className="mt-4" onSubmit={(e) => e.preventDefault()}>
-            <FormGroup>
-              <Label for="passphrase">
-                <small>
-                  <b>{'Passphrase'}</b>
-                </small>
-              </Label>
-              <Input
-                id="passphrase"
-                type="password"
-                name="passphrase"
-                data-testid="passphrase"
-                value={passphrase}
-                onChange={changePassphrase}
-                valid={passphraseValid}
-                invalid={passphraseInvalid}
-                placeholder="Enter the passphrase"
-                // autoComplete="new-password"
-                autoComplete="off"
-                maxLength={32}
-                minLength={8}
-              />
-              <FormFeedback>{'invalid passphrase'}</FormFeedback>
-              <FormFeedback valid={true}>{'valid passphrase'}</FormFeedback>
-            </FormGroup>
-            <br />
-
-            <div className="px-3">
-              <FormGroup inline={true}>
-                <Label check={isDisclaimerChecked} onChange={handleCheck}>
-                  <Input type="checkbox" /> <Disclaimer />
-                </Label>
-              </FormGroup>
-              <div className="text-center">
-                <Button
-                  text={'Confirm'}
-                  type="primary"
-                  ariaLabel={'Confirm'}
-                  onClick={() => setCurrentStep(SECOND_STEP)}
-                  disabled={isDisabled}
-                />
-              </div>
-            </div>
-          </Form>
-        </div>
-      </div>
-    );
-  };
-
-  const renderFinalStep = () => {
-    return (
-      <div>
-        <div className="text-center">
-          <h2 className="pt-5">
-            <b>{'Please Save Your Private Key'}</b>
-          </h2>
-          <p className="text-secondary py-3">
-            {`Your new wallet has been created.`}
-            <br />
-            {`Make sure to copy the private key below and save it.`}
-          </p>
-        </div>
-        <small>
-          <b>Private Key</b>
-        </small>
-        <p className="text-primary py-3">
-          <code className="text-primary">{privateKey}</code>
-        </p>
-      </div>
-    );
-  };
-
-  const renderKeystoreStep = () => {
-    const isPending = encryptStatus === requestStatus.PENDING;
-    const buttonText = isPending ? 'Generating Keystore File' : 'Generate Keystore File';
-    return (
-      <div>
-        <div className="text-center">
-          <h2 className="pt-5">
-            <b>{'Generate Keystore File'}</b>
-          </h2>
-          <p className="text-secondary py-3">
-            {`The password for your keystore file for a new wallet has been set. Please click the tab below to generate your keystore to setup your wallet and move on to the last step.`}
-          </p>
-        </div>
-        <div className="py-4 text-center">
-          <Button
-            text={buttonText}
-            type="primary"
-            ariaLabel={buttonText}
-            onClick={generateKeystore}
-            before={
-              isPending ? (
-                <span className="pr-1">
-                  <Spinner size="small" />
-                </span>
-              ) : null
-            }
-            disabled={isPending}
-          />
-        </div>
-      </div>
-    );
-  };
-
+  const isPending = encryptStatus === requestStatus.PENDING;
+  const buttonText = isPending ? 'Generating Keystore File' : 'Generate Keystore File';
   return (
     <div>
-      <Card>
-        <div className="py-5">
-          <Row>
-            <Col xs={11} sm={11} md={10} lg={8} className="mr-auto ml-auto">
-              <Steps size="small" current={currentStep}>
-                <Step title="Passphrase" />
-                <Step title="Keystore" />
-                <Step title="Complete" />
-              </Steps>
-            </Col>
-            <Col xs={10} sm={10} md={8} lg={7} className="mr-auto ml-auto">
-              {currentStep === FIRST_STEP ? renderPassphraseStep() : null}
-              {currentStep === SECOND_STEP ? renderKeystoreStep() : null}
-              {currentStep === FINAL_STEP ? renderFinalStep() : null}
-            </Col>
-          </Row>
-        </div>
-      </Card>
+      <div className="text-center">
+        <h2 className="pt-5">
+          <b>{'Generate Keystore File'}</b>
+        </h2>
+        <p className="text-secondary py-3">
+          {`The password for your keystore file for a new wallet has been set. Please click the tab below to generate your keystore to setup your wallet and move on to the last step.`}
+        </p>
+      </div>
+      <div className="py-4 text-center">
+        <Button
+          text={buttonText}
+          type="primary"
+          ariaLabel={buttonText}
+          onClick={generateKeystore}
+          before={
+            isPending ? (
+              <span className="pr-1">
+                <Spinner size="small" />
+              </span>
+            ) : null
+          }
+          disabled={isPending}
+        />
+      </div>
+    </div>
+  );
+};
+
+const FinalStep = ({ privateKey }) => {
+  return (
+    <div>
+      <div className="text-center">
+        <h2 className="pt-5">
+          <b>{'Please Save Your Private Key'}</b>
+        </h2>
+        <p className="text-secondary py-3">
+          {`Your new wallet has been created.`}
+          <br />
+          {`Make sure to copy the private key below and save it.`}
+        </p>
+      </div>
+      <small>
+        <b>Private Key</b>
+      </small>
+      <p className="text-primary py-3">
+        <code className="text-primary">{privateKey}</code>
+      </p>
     </div>
   );
 };
