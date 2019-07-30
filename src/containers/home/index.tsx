04dc20c3ec92032cee77a4530f15b15efced8da1
noelyoo <noelyoo.dev@gmail.com>
2019-07-30 17:15:36 +0900

    chore: clean up styles

--- src/containers/home/index.tsx
+++ src/containers/home/index.tsx
@@ -33,33 +33,7 @@ const Container = styled.div`
   color: #fff;
   background: linear-gradient(-45deg, #4c54d2, #fe524a, #4c54d2);
   background-size: 400% 400%;
-  -webkit-animation: Gradient 15s ease infinite;
-  -moz-animation: Gradient 15s ease infinite;
   animation: Gradient 15s ease infinite;
-
-  @-webkit-keyframes Gradient {
-    0% {
-      background-position: 0% 50%;
-    }
-    50% {
-      background-position: 100% 50%;
-    }
-    100% {
-      background-position: 0% 50%;
-    }
-  }
-
-  @-moz-keyframes Gradient {
-    0% {
-      background-position: 0% 50%;
-    }
-    50% {
-      background-position: 100% 50%;
-    }
-    100% {
-      background-position: 0% 50%;
-    }
-  }
 
   @keyframes Gradient {
     0% {
