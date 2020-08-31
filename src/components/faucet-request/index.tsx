0fa22d2b3e38de46284cc1c4bcc1144a02599c88
chainvisions <67360117+chainvisions@users.noreply.github.com>
2020-08-30 21:34:47 -0600

    Update index.tsx

--- src/components/faucet-request/index.tsx
+++ src/components/faucet-request/index.tsx
@@ -44,7 +44,7 @@ const FaucetRequest = ({ faucet, toAddress, reset }) => {
             <small className="text-danger text-fade-in">
               {error.message}
               <br />
-              {'Google reCAPTCHA might not work for some country.'}
+              {'Google reCAPTCHA might not work for some countries.'}
             </small>
           </p>
           <br />
