7b5aa1fecf102e63dc3aead713f6ac85ef036811
noel <noel@zilliqa.com>
2021-08-21 12:54:18 +0900

    test: fix snapshot testing

--- src/components/footer/index.test.tsx
+++ src/components/footer/index.test.tsx
@@ -14,23 +14,12 @@
  * You should have received a copy of the GNU General Public License along with
  * nucleus-wallet.  If not, see <http://www.gnu.org/licenses/>.
  */
-import '@testing-library/react/cleanup-after-each';
-import 'jest-styled-components';
-import 'jest-dom/extend-expect';
 
-/* tslint:disable */
-// this is just a little hack to silence a warning that we'll get until react
-// fixes this: https://github.com/facebook/react/pull/14853
-const originalError = console.error;
-beforeAll(() => {
-  console.error = (...args) => {
-    if (/Warning.*not wrapped in act/.test(args[0])) {
-      return;
-    }
-    originalError.call(console, ...args);
-  };
-});
+import React from 'react';
+import { render } from '@testing-library/react';
+import Footer from '.';
 
-afterAll(() => {
-  console.error = originalError;
+test('matches the snapshot', () => {
+  const { container } = render(<Footer year={1} />);
+  expect(container.firstChild).toMatchSnapshot();
 });
